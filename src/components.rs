c2166392be9e33f33635030f89e55aabb6dca382
angelovangel <aangeloo@gmail.com>
2026-04-29 08:11:42 +0300

    ui

--- src/components.rs
+++ src/components.rs
@@ -15,7 +15,7 @@ pub fn app_title(filescount: Signal<usize>) -> Element {
             if *filescount.read() == 0 {
                 p{
                 "This application runs basic analysis on sequencing files in fastq format.
-                Select fastq or fastq.gz files to analyse."
+                Select fastq or fastq.gz files to analyse (no data leaves the browser)."
                 }
             }
         }
